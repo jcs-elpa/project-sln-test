04ce46750ebcac29a943b55d6b8312f40078039b
jenchieh <jcs090218@gmail.com>
2020-06-05 23:55:23 +0800

    Add more helper scripts.

--- scripts/reset.bat
+++ scripts/reset.bat
@@ -1,13 +1,15 @@
 @echo off
 :: ========================================================================
-:: $File: setup.bat $
-:: $Date: 2020-06-05 23:40:36 $
+:: $File: reset.bat $
+:: $Date: 2020-06-05 23:47:11 $
 :: $Revision: $
 :: $Creator: Jen-Chieh Shen $
 :: $Notice: See LICENSE.txt for modification and distribution information
 ::                   Copyright © 2020 by Shen, Jen-Chieh $
 :: ========================================================================
 
+set VC_PATH=".git"
+
 cd ..
 cd project_csharp
-git init
+rmdir %VC_PATH% /s /q
--- scripts/reset.sh
+++ scripts/reset.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 # ========================================================================
-# $File: setup.sh $
-# $Date: 2020-06-05 23:43:27 $
+# $File: reset.sh $
+# $Date: 2020-06-05 23:47:28 $
 # $Revision: $
 # $Creator: Jen-Chieh Shen $
 # $Notice: See LICENSE.txt for modification and distribution information
 #                   Copyright © 2020 by Shen, Jen-Chieh $
 # ========================================================================
 
+VC_PATH=".git"
+
 cd ..
 cd project_csharp
-git init
+rm -rf %VC_PATH%
