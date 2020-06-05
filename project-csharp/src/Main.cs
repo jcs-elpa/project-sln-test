780b6ab6f8fa048430ef8249bcbb40c312bf2201
jenchieh <jcs090218@gmail.com>
2020-06-05 23:37:51 +0800

    Add csharp project.

---
+++ project-csharp/src/Main.cs
@@ -0,0 +1,21 @@
+/**
+ * $File: Main.cs $
+ * $Date: 2019-11-12 11:04:56 $
+ * $Revision: $
+ * $Creator: Jen-Chieh Shen $
+ * $Notice: See LICENSE.txt for modification and distribution information
+ *                   Copyright © 2019 by Shen, Jen-Chieh $
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    /* Variables */
+
+    /* Setter/Getter */
+
+    /* Functions */
+
+}
---
+++ project-csharp/src/gameobject.js
@@ -0,0 +1,10 @@
+/**
+ * $File: gameobject.js $
+ * $Date: 2019-11-13 13:33:36 $
+ * $Revision: $
+ * $Creator: Jen-Chieh Shen $
+ * $Notice: See LICENSE.txt for modification and distribution information
+ *                   Copyright © 2019 by Shen, Jen-Chieh $
+ */
+
+"use strict";
