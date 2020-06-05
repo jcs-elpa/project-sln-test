8147c41078547e46ef2fc79bc8eb13a02ddf4614
jenchieh <jcs090218@gmail.com>
2020-06-05 23:46:30 +0800

    Add scripts.

---
+++ scripts/setup.bat
@@ -0,0 +1,14 @@
+@echo off
+:: ========================================================================
+:: $File: setup.bat $
+:: $Date: 2020-06-05 23:40:36 $
+:: $Revision: $
+:: $Creator: Jen-Chieh Shen $
+:: $Notice: See LICENSE.txt for modification and distribution information
+::                   Copyright © 2020 by Shen, Jen-Chieh $
+:: ========================================================================
+
+cd ..
+
+cd project_csharp
+git init
---
+++ scripts/setup.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# ========================================================================
+# $File: setup.sh $
+# $Date: 2020-06-05 23:43:27 $
+# $Revision: $
+# $Creator: Jen-Chieh Shen $
+# $Notice: See LICENSE.txt for modification and distribution information
+#                   Copyright © 2020 by Shen, Jen-Chieh $
+# ========================================================================
